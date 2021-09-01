b579a04956adf48d62bdd2ec585efe34329b355c
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-09-01 23:46:20 +0900

    Add files via upload

---
+++ compare.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<?php
+echo "1==2 : ";
+var_dump(1==2);           #false
+echo '<br />';
+echo "1==1 : ";
+var_dump(1==1);           #true
+echo '<br />';
+echo '"one"=="two" : ';
+var_dump("one"=="two");   #false
+echo '<br />';
+echo '"one"=="one" : ';
+var_dump("one"=="one");   #true
+echo '<br />';
+echo "1!=2 : ";
+var_dump(1!=2);           #true
+echo '<br />';
+echo "1!=1 : ";
+var_dump(1!==1);           #false
+echo '<br />';
+echo '"one"!="two" : ';
+var_dump("one"!="two");   #true
+echo '<br />';
+echo '"one"!="one" : ';
+var_dump("one"!="one");   #false
+echo '<br />';
+?>
+</body>
+</html>
