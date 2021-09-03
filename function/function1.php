f2fce79a70a713a22f46a1946ec617a40506b6ee
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-09-03 22:48:18 +0900

    Add files via upload

---
+++ function/function1.php
@@ -0,0 +1,14 @@
+<?php
+function numbering () {
+    $i = 0;
+    while($i<10){
+        echo $i;
+        $i += 1;
+    }
+}
+numbering();
+numbering();
+numbering();
+numbering();
+numbering();
+?>
---
+++ function/function2.php
@@ -0,0 +1,7 @@
+<?php
+function get_arguments($arg1, $arg2){
+    return $arg1 + $arg2;
+}
+echo get_arguments(10, 20);
+echo get_arguments(20, 30);
+?>
