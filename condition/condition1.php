edf0d77fe120f1932d08b78aba4c40d9fcf06d0b
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-09-03 22:47:31 +0900

    Add files via upload

---
+++ condition/condition1.php
@@ -0,0 +1,15 @@
+<?php
+if(true){
+    echo 'result : true';
+}
+echo '<br />';
+if (false){
+    echo 1;
+} else if (false){
+    echo 2;
+} else if (false){
+    echo 3;
+} else {
+    echo 4;
+}
+?>
