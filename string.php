292c050df42e45b521c54f75871895ddd77be51f
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-09-08 23:12:45 +0900

    Add files via upload

--- string.php
+++ string.php
@@ -1,44 +1,6 @@
 <?php
-echo "그는 \"안녕하세요 라고 말했다\"";
+$a = array('진인사대천명', 'jininsadachunmung');
+echo "권현성의의 인생모토는 {$a[0]} {$a[1]}입니다\n";
 echo '<br />';
-$a = 445;
-echo $a + 555;
-echo '<br />';
-$first = "coding";
-echo $first." everybody";
-echo '<br />';
-echo 100+10;
-echo '<br />';
-echo (100+10)/10;
-echo '<br />';
-echo ((100+10)/10)-10;
-echo '<br />';
-echo (((100+10)/10)-10)*10;
-echo '<br />';
-/*변수*/
-$a = 100;
-$a = $a + 10;
-print $a.'<br />';
-$a = $a / 10;
-print $a.'<br />';
-$a = $a - 10;
-print $a.'<br />';
-$a = $a * 10 ;    
-print $a.'<br />';
-/*상수의 장점*/
-define('TITLE', 'PHP Tutorial');
-echo TITLE;
-define('TITLE', 'JAVA Tutorial');
-/*고급 주제_ 변수와 상수 */
-$a = 100;
-echo gettype($a);
-settype($a, 'double');
-echo '<br />';
-echo gettype($a);
-/* 가변 변수 */
-echo '<br />';
-$title = 'subject';
-$$title = 'PHP tutorial';
-echo $subject;
+echo '현성이의 좌우명은 '.$a[0].' '.$a[1].'입니다';
 ?>
-
